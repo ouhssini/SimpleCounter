8260dcb9e3391a77adec9ce32e85424c3fc9da82
ouhssini <taghbalte2015@gmail.com>
2023-07-28 22:12:56 +0100

    Change JAVASCRIPT FOREACH STATMENT

--- Script.js
+++ Script.js
@@ -2,20 +2,22 @@ let counter = 0;
 
 let value = document.querySelector("#value");
 let btns = document.querySelectorAll(".btn");
-
-btns[0].onclick = () => {
-  counter--;
-  value.textContent = counter;
-  value.style.color = "rgb(224, 48, 48)";
-};
-btns[1].onclick = () => {
-  counter = 0;
-  value.textContent = counter;
-  value.style.color = "rgb(43, 42, 42)";
-};
-
-btns[2].onclick = () => {
-  counter++;
-  value.textContent = counter;
-  value.style.color = "rgb(62, 228, 62)";
-};
+btns.forEach((btn, index) => {
+  btn.onclick = () => {
+    if (index === 0) {
+      counter--;
+    } else if (index === 1) {
+      counter = 0;
+    } else if (index === 2) {
+      counter++;
+    }
+    if (counter < 0) {
+      value.style.color = "rgb(224, 48, 48)"; // Red color
+    } else if (counter > 0) {
+      value.style.color = "rgb(62, 228, 62)"; // green color
+    } else {
+      value.style.color = "rgb(43, 42, 42)";
+    }
+    value.textContent = counter;
+  };
+});
